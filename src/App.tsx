65390be7b7a94aa157449705a06f15d28014e4f4
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-03-29 00:30:30 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './Pages/Home'
+import { GlobalStyles } from './GlobalStyles'
+
+function App() {
+  
+  return (
+    <>
+      <GlobalStyles/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/GlobalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles=createGlobalStyle`
+   *{
+     margin:0;
+     padding:0;
+     box-sizing:border-box;
+   }
+   body{
+     height:100vh;
+     background-color:#F4F6F8;
+     font-family: 'Kumbh Sans', sans-serif;
+   }
+`
